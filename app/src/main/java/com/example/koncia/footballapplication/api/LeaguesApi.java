5795d8cb0e49dddd542a084908f030e0ddc5946e
Koncia <michalglocki@gmail.com>
2018-06-03 22:50:04 +0200

    api configrutation class

---
+++ app/src/main/java/com/example/koncia/footballapplication/api/LeaguesApi.java
@@ -0,0 +1,17 @@
+package com.example.koncia.footballapplication.api;
+
+import com.example.koncia.footballapplication.models.League;
+
+import java.util.List;
+
+import io.reactivex.Flowable;
+import io.reactivex.Single;
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface LeaguesApi {
+    String BASE_URL = "http://api.football-data.org/v1/";
+
+    @GET("soccerseasons")
+    Single<List<League>> getLeagues();
+}
